b17fb0856744c1a3576921d81bedfdcfd8fd7723
wandol <wandol@kakao.com>
2021-06-23 23:47:43 +0900

    filter add

--- src/main/java/com/example/firstservice/FirstController.java
+++ src/main/java/com/example/firstservice/FirstController.java
@@ -1,15 +1,25 @@
 package com.example.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/first-service")
+@Slf4j
 public class FirstController {
 
     @GetMapping("/welcome")
     public String welcome(){
         return "welcome to the first service";
     }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header){
+        log.info(header);
+        return "hello world in first service";
+    }
 }
+
